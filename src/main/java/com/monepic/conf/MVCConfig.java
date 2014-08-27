7cc7fb63306f316ad3c9ec38f50d6afa32ade3cc
Ed <ed@monepic.com>
2014-08-27 04:04:10 +0100

    jackson

--- src/main/java/com/monepic/conf/MVCConfig.java
+++ src/main/java/com/monepic/conf/MVCConfig.java
@@ -1,18 +1,27 @@
 package com.monepic.conf;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
 @EnableWebMvc
+@EnableHypermediaSupport(type = { HypermediaType.HAL })
 @ComponentScan(basePackages = {"com.monepic"})
-public class MVCConfig {
+public class MVCConfig extends WebMvcConfigurerAdapter {
 	
 	Logger log = LoggerFactory.getLogger(getClass());
 	@PostConstruct public void init() {
@@ -21,6 +30,22 @@ public class MVCConfig {
 		log.debug("LOG DEBUG");
 		log.trace("LOG TRACE");
 	}
+	
+	
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jacksonMessageConverter());
+    }
+ 
+    @Bean
+    public MappingJackson2HttpMessageConverter jacksonMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setPrefixJson(false);
+        converter.setPrettyPrint(true);
+   //     converter.setObjectMapper(objectMapper());
+        return converter;
+    }
 
 	   
 }
